a6cc16c2ac31b84037b2a5c25a69e718002b6c6c
NodeStake <info@nodestake.top>
2023-02-09 15:48:54 +0800

    disable coffee

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,7 +1,6 @@
 <template>
   <p class="clearfix mb-0">
     <span class="float-md-left d-none d-md-block d-md-inline-block mt-25">
-      Powered By
       <b-link
         class="ml-25 font-weight-bolder"
         href="#"
@@ -9,22 +8,6 @@
       >NodeStake</b-link>
     </span>
 
-    <router-link
-      v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-      class="float-md-right"
-      to="/coffee"
-    >Buy me a cup of coffee.
-      <span
-        v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-        variant="outline-primary"
-      >
-        <feather-icon
-          icon="HeartIcon"
-          size="21"
-          class="text-danger stroke-current"
-        />
-      </span>
-    </router-link>
   </p>
 </template>
 
