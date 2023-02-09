f6843725728741a8ce2fa303bb4a8bd95e3031b9
NodeStake <info@nodestake.top>
2023-02-09 14:36:59 +0800

    Revert "Update index.js"
    This reverts commit 41c189058d0f04df46d6e6767899564c1afffbd8.

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,7 +19,7 @@ function processMenu() {
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: 'ChromeIcon',
+      icon: 'https://ping.pub/logo.png',
     })
   }
   chainMenus.push({ header: 'LINKS' })
