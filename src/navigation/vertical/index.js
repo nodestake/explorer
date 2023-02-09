41c189058d0f04df46d6e6767899564c1afffbd8
NodeStake <info@nodestake.top>
2023-02-09 14:22:31 +0800

    Update index.js
    Change icon

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,7 +19,7 @@ function processMenu() {
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: 'https://ping.pub/logo.png',
+      icon: 'ChromeIcon',
     })
   }
   chainMenus.push({ header: 'LINKS' })
